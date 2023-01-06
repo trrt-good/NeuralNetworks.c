a20fb544057ad17d3e367de069d53c3688a2050a
trrt-good <t.atwood.rose@gmail.com>
2023-01-05 21:55:06 -0800

    formatting

--- DataReader/dataReader.c
+++ DataReader/dataReader.c
@@ -24,7 +24,78 @@ void swap(float** arr, int i, int j)
     arr[j] = temp;
 }
 
-int readRowData_ML(char* fileName, char* delimiter, int bufferSize, int totalDataPoints, int numTesting, int numInputs, int numOutputs, float **trainingInputArr, float **trainingOutputsArr, float **testingInputArr, float **testingOutputArr, int randomizeOrder)
+int read_iris_data(char* fileName, char* delimiter, int bufferSize, int totalDataPoints, int numTesting, int numInputs, int numOutputs, float **trainingInputArr, float **trainingOutputsArr, float **testingInputArr, float **testingOutputArr, int randomizeOrder)
+{
+    srand(time(NULL));
+    float** allInputs = laa_allocMatrix(totalDataPoints, numInputs, 0);
+    float** allOutputs = laa_allocMatrix(totalDataPoints, numOutputs, 0);
+    char strBuffer[bufferSize];
+    char* stringToken;
+    int i, j, nthExample = 0;
+    int randIndex;
+        
+    FILE *filePointer; 
+
+    filePointer = fopen(fileName, "r");
+    if (filePointer == NULL)
+    {
+        printf("could not find file! %s", fileName);
+    }
+
+    while(fgets(strBuffer, bufferSize+1, filePointer) != NULL && nthExample < totalDataPoints)
+    {
+        stringToken = strtok(strBuffer, delimiter);
+        for (i = 0; i < numInputs && stringToken != NULL; i ++)
+        {
+            allInputs[nthExample][i] = (float)atof(stringToken);
+            
+            stringToken = strtok(NULL, delimiter);
+        }
+        for (i = 0; i < numOutputs && stringToken != NULL; i ++)
+        {
+            allOutputs[nthExample][i] = (float)atof(stringToken);
+        
+            stringToken = strtok(NULL, delimiter);
+        }
+        if (randomizeOrder)
+        {
+            randIndex = (nthExample)? nthExample-rand()%nthExample : 0;
+            swap(allInputs, nthExample, randIndex);
+            swap(allOutputs, nthExample, randIndex);
+        }
+        nthExample++;
+    }
+    for (i = 0; i < totalDataPoints-numTesting; i ++)
+    {
+        for (j = 0; j < numInputs; j++)
+        {
+            trainingInputArr[i][j] = allInputs[i][j];
+        }
+        for (j = 0; j < numOutputs; j++)
+        {
+            trainingOutputsArr[i][j] = allOutputs[i][j];
+        }
+    }
+    if (numTesting)
+    {
+        for (i = totalDataPoints-numTesting; i < totalDataPoints; i ++)
+        {
+            for (j = 0; j < numInputs; j++)
+            {
+                testingInputArr[i-totalDataPoints+numTesting][j] = allInputs[i][j];
+            }
+            for (j = 0; j < numOutputs; j++)
+            {
+                testingOutputArr[i-totalDataPoints+numTesting][j] = allOutputs[i][j];
+            }
+        }
+    }
+    laa_freeMatrix(allInputs, totalDataPoints);
+    laa_freeMatrix(allOutputs, totalDataPoints);
+    fclose(filePointer);
+}
+
+int read_iris_data(char* fileName, char* delimiter, int bufferSize, int totalDataPoints, int numTesting, int numInputs, int numOutputs, float **trainingInputArr, float **trainingOutputsArr, float **testingInputArr, float **testingOutputArr, int randomizeOrder)
 {
     srand(time(NULL));
     float** allInputs = laa_allocMatrix(totalDataPoints, numInputs, 0);
